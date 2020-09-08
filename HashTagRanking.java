f16aded4e621e5da733be860f898fd7b4c520a89
sreeharipullasetty <33320657+sreeharipullasetty@users.noreply.github.com>
2020-09-08 12:52:04 +0530

    Update HashTagRanking.java

--- HashTagRanking.java
+++ HashTagRanking.java
@@ -15,20 +15,26 @@ public class HashTagRanking {
 		String str;
 		System.out.print("Tweet: ");
 		HashMap<String, Integer> hashTags = new HashMap<>();
+		
+		//Reading the tweets from the user.
 		while (!(str = br.readLine()).equals("exit")) {
 			System.out.print("Tweet: ");
-			int startIndex = 0;
+			int startIndex = 0;// startIndex will be the hashTag Starting position.
+			
+			// The below while loop will retrieve all the hashTag from the input line.
 			while ((startIndex = str.indexOf("#", startIndex)) != -1) {
-				int endIndex = str.indexOf(' ', startIndex);
+				int endIndex = str.indexOf(' ', startIndex);// endIndex will be the hashTag Ending Position and it is -1 if the hashTag is at the end of String.
 				String hashTag = "";
 				if (endIndex == -1) {
-					hashTag = str.substring(startIndex + 1);
+					hashTag = str.substring(startIndex + 1); //Retriving the hashTag
 					startIndex = str.length();
 				} else {
-					hashTag = str.substring(startIndex + 1, endIndex);
+					hashTag = str.substring(startIndex + 1, endIndex); //Retriving the hashTag
 					startIndex = endIndex;
 				}
-				hashTag = hashTag.toLowerCase();
+				hashTag = hashTag.toLowerCase(); // converting it to lowercase to uniqly identify hashTag
+				
+				//adding the hashTag to Map.
 				if (hashTag.length() != 0) {
 					if (hashTags.containsKey(hashTag)) {
 						hashTags.put(hashTag, hashTags.get(hashTag) + 1);
@@ -51,12 +57,15 @@ public class HashTagRanking {
 	}
 
 	public static List<Map.Entry<String, Integer>> getTopHashTags(HashMap<String, Integer> hashTags) {
-		List<Map.Entry<String, Integer>> list = new LinkedList<Map.Entry<String, Integer>>(hashTags.entrySet());
+		List<Map.Entry<String, Integer>> list = new LinkedList<Map.Entry<String, Integer>>(hashTags.entrySet());//storing the hashmap entries to list
 		Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+			//sorting the entries based on value in descending order.
 			public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
 				return (o2.getValue()).compareTo(o1.getValue());
 			}
 		});
+		
+		//returning the result list.
 		if (list.size() >= 10) {
 			return list.subList(0, 10);
 		} else {
